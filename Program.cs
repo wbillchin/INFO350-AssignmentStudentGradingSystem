f0c5f69da93779bb4868b70e83634a77c45e84c6
Bill Chin <chinww@vcu.edu>
2024-09-25 17:56:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * INFO 350 - Student Grading System
+ * 
+ * <replace with your name>
+ * 
+ * <replace with something appropriate>
+ * 
+ * Modify this code to complete this assignment
+ */
+
+using System;
+class StudentGradingSystem
+{
+    static void Main(string[] args)
+    {
+        int[] homeworkGrades = { 92, 0, 78, 82, 88, 0, 100, 87, 99, 91 };  // 10 homework assignments, max 100 each
+        int[] examGrades = { 78, 96 };  // 2 exams, scored out of 100
+
+        // Constants for number of homework assignments and exams
+        const int homeworkCount = 10;
+        const int examCount = 2;
+
+
+
+    }
+}
